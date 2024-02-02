358a057ff2c292b87335269be37d57b327f5740b
Beremane <158621460+Beremane@users.noreply.github.com>
2024-02-02 22:31:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,17 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+         <div id="odin-chatbot"></div>
+      <script>
+        window.project_id = "Wx5xi6sprMXQKxh2cWIQ";
+        window.frame_type = "window";
+      </script>
+      <script
+        src="https://app.getodin.ai/loader.min.js"
+        project_id="Wx5xi6sprMXQKxh2cWIQ"
+        frame_type="window"
+        defer
+      ></script>
       </main>
 
       <Footer />
